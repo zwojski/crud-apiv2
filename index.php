4b5995f26aefdbec087094f3228c98bb4934d988
Pawel <zwojski@gmail.com>
2024-03-21 16:08:01 +0100

    crud-apiv2

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/controllers/$class.php";
+});
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/services/$class.php";
+});
+
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
+
+
+header("Content-type: application/json; charset=UTF-8");
+
+
+$parts = explode("/" , $_SERVER["REQUEST_URI"]);
+
+if ($parts[1] != "products") {
+    http_response_code(404);
+    exit;
+}
+
+$id = $parts[2] ?? null;
+
+$database = new Database("localhost", "product_db", "root", "12345!");
+
+$gateway = new ProductService($database);
+
+$controller = new ProductController($gateway);
+
+$controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
+
+
